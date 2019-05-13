21549f0e4b61a98bfb15dbf36a4e81937547157c
peaugust <peaugust@hotmail.com>
2019-05-12 23:40:17 -0300

    Adding '/' route

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.route('/')
+    .get((req, res) => {
+       res.send('OK! Tudo funcionando!'); 
+    })
+    
 app.route('/books')
 .get((req, res) => {
     res.json([{
