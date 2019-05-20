a6328f6734a0b109088aa0fe46e64cbe848785b2
peaugust <peaugust@hotmail.com>
2019-05-20 20:49:57 -0300

    creating mongo connection

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dbConfig from './config/database.config';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.route('/')
-    .get((req, res) => {
-       res.send('OK! Tudo funcionando!'); 
-    })
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Tudo funcionando! OK!"});
+});
+
+// listen for requests
+// app.listen(7000, () => {
+//     console.log("Server is listening on port 3000");
+// });
+// app.route('/')
+//     .get((req, res) => {
+//         res.send('OK! Tudo funcionando!'); 
+//     })
     
-app.route('/books')
-.get((req, res) => {
-    res.json([{
-        id: 1,
-        name: 'Default Book'
-    }])
-})
+// app.route('/books')
+// .get((req, res) => {
+//     res.json([{
+//         id: 1,
+//         name: 'Default Book'
+//     }])
+// })
+
+
 export default app;
