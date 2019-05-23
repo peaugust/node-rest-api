44896cf3ed7bae00421610feca44df8a470dc52e
peaugust <peaugust@hotmail.com>
2019-05-23 01:33:14 -0300

    remove deprecated declaration

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb://localhost/noderestapi', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/noderestapi', { useNewUrlParser:true });
 mongoose.Promise = global.Promise;
 
 export default mongoose;
