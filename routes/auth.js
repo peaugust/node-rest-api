2eeac27c3854f34c77f45c8c5e664877a635f81c
peaugust <peaugust@hotmail.com>
2019-05-23 01:34:28 -0300

    add auth route

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import User from './../models/Users'
+
+const router = express.Router();
+
+router.post('/register', async( req, res ) => {
+    try{
+        const user = await User.create(req.body);
+        return res.send({ user });
+    } catch (err) {
+        return res.status(400).send({ error: 'Registration Failed' });
+    }
+});
+
+
+export default router;
